8a6f7b6e0b25de210b1b6ec6c959856e6838996f
Lakshitapatidar <108969093+Lakshitapatidar@users.noreply.github.com>
2023-02-25 23:20:52 +0530

    Add files via upload

---
+++ cpp_task1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<a<<"is graeter";
+    }
+    else
+    {
+        cout<<b<<"is graeter";
+    }
+}
